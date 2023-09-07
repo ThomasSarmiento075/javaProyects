3827343943b00cdde31004500836191b0d5a9bf3
ThomasSarmiento075 <143768007+ThomasSarmiento075@users.noreply.github.com>
2023-09-07 11:44:06 -0500

    Add files via upload

---
+++ primerosCodigos/Tree.java
@@ -0,0 +1,22 @@
+class Tree
+{
+public static void main(String args[]) {
+System.out.println("Here there, I'm Thomas and I'm learning to code in Java!");
+System.out.println("I'm going to plant a Tree today! or Ready to get my hands dirty!");
+System.out.println("       *    ");
+System.out.println("      ***    ");
+System.out.println("     *****    ");
+System.out.println("    *******    ");
+System.out.println("   *********    ");
+System.out.println("  ***********    ");
+System.out.println("   *********    ");
+System.out.println("    *******    ");
+System.out.println("     *****    ");
+System.out.println("       *    ");
+System.out.println("       *    ");
+System.out.println("       *    ");
+System.out.println("       *    ");
+System.out.println("       *    ");
+System.out.println("   *********   ");
+}
+}
---
+++ primerosCodigos/doubleExample.java
@@ -0,0 +1,6 @@
+public class doubleExample{
+public static void main(String[] args){
+double androidShare=-81.7;
+System.out.println(androidShare);
+}
+}
