196030fd2382aae6e4d5531cc73b8b682601d66a
ThomasSarmiento075 <143768007+ThomasSarmiento075@users.noreply.github.com>
2023-09-07 13:39:45 -0500

    Add files via upload

---
+++ primerosCodigos/stringExample.java
@@ -0,0 +1,6 @@
+public class stringExample {
+public static void main(String[] args) {
+String openingLyrics="Yesterday, a11 my troubles seemed so far away";
+System.out.println(openingLyrics);
+}
+}
---
+++ primerosCodigos/variables1.java
@@ -0,0 +1,8 @@
+public class variables1{
+public static void main(String[] args){
+String name="Thomas Sarmiento";
+int yearCreated=2005;
+System.out.println(name);
+System.out.println(yearCreated);
+}
+}
