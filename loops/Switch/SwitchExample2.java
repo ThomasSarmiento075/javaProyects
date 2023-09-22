fdb36c09e93ffca98415faea87320e999f69a556
ThomasSarmiento075 <143768007+ThomasSarmiento075@users.noreply.github.com>
2023-09-22 15:17:19 -0500

    Add files via upload

---
+++ loops/Switch/SwitchExample2.java
@@ -0,0 +1,10 @@
+public class SwitchExample2 {  
+public static void main(String[] args) {  
+int number=20;   
+switch(number){   
+case 10: System.out.println("10");  
+case 20: System.out.println("20");  
+case 30: System.out.println("30");  
+}  
+}  
+}  
