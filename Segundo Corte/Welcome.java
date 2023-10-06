c01a26d3543d5c177c3a13027a2d3f358aa0eb82
ThomasSarmiento075 <143768007+ThomasSarmiento075@users.noreply.github.com>
2023-10-05 22:32:17 -0500

    Add files via upload

---
+++ Segundo Corte/Welcome.java
@@ -0,0 +1,11 @@
+package welcome;
+import javax.swing.JOptionPane;
+public class Welcome {
+ 
+    public static void main(String[] args) {
+       JOptionPane.showMessageDialog(null, "¡Welcome to the\n Java world!");
+       System.exit(0);
+     
+    }
+    
+}
