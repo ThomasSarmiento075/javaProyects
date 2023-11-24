1c93434222c8c321ece28638d1ac3eb20b320352
ThomasSarmiento075 <143768007+ThomasSarmiento075@users.noreply.github.com>
2023-11-24 18:47:01 -0500

    Add files via upload

---
+++ Segundo Corte/Herencia/test/TestHerencia.java
@@ -0,0 +1,13 @@
+import herencia.*;
+import java.util.Date;
+public class TestHerencia
+{
+	public static void main(String[] args)
+	{
+		Empleado empleado1 = new Empleado("Alex", 3000);
+		System.out.println(empleado1);
+		Empleado empleado2 = new Empleado("Alex", 4000);
+		System.out.println(empleado2);
+	}
+	
+}
