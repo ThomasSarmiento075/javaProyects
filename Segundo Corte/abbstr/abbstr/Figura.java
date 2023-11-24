fd7e0225f4485ec7685a631ca39e359df4c1214a
ThomasSarmiento075 <143768007+ThomasSarmiento075@users.noreply.github.com>
2023-11-24 16:44:32 -0500

    Add files via upload

---
+++ Segundo Corte/abbstr/abbstr/Figura.java
@@ -0,0 +1,13 @@
+public abstract class Figura {
+	private String color;
+
+	public Figura(String color) {
+		this.color = color;
+	}
+	
+	public abstract double calcularArea();
+
+	public String getColor() {
+		return color;
+	}		
+}
