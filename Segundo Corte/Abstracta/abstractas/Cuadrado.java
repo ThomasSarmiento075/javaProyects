52b392b9e8ef1a3f93f86e296928838cd3bcfd79
ThomasSarmiento075 <143768007+ThomasSarmiento075@users.noreply.github.com>
2023-11-24 17:17:04 -0500

    Add files via upload

---
+++ Segundo Corte/Abstracta/abstractas/Cuadrado.java
@@ -0,0 +1,12 @@
+public class Cuadrado extends FiguraGeometrica {
+
+    public Cuadrado(String tipoFigura) {
+        super(tipoFigura);
+    }
+    
+    @Override
+    public void dibujar() {
+        System.out.println("Se dibuja un: " + this.getClass().getSimpleName());
+    
+    }    
+}
---
+++ Segundo Corte/Abstracta/abstractas/Rectangulo.java
@@ -0,0 +1,11 @@
+public class Rectangulo extends FiguraGeometrica {
+
+    public Rectangulo(String tipoFigura) {
+        super(tipoFigura);
+    }
+    
+    @Override
+    public void dibujar(){
+        System.out.println("Se dibuja un:" + this.getClass().getSimpleName());
+    }
+}
